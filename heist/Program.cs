5c367e03d4e5ac14c34cce057cfa95d65274aeb2
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-09-26 12:06:30 -0400

    Fix the loop logic with double equals signs and adding the right name variable to new member obj

--- heist/Program.cs
+++ heist/Program.cs
@@ -11,27 +11,35 @@ namespace heist
 
             //update to store several team members', stop adding members when a blank name is entered, display message with number of members in team, display each team member's info.
 
-            TeamMember getMember = new TeamMember();
             List<TeamMember> teamMembers = new List<TeamMember>();
 
-            do
+            int i = 0;
+            while (i < 100)
             {
-
                 Console.WriteLine("Add team member. What is their name?");
 
-                getMember.Name = Console.ReadLine();
+                string name = Console.ReadLine();
 
-                Console.WriteLine("What is their skill level (positive integer)?");
+                if (name == " ")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("What is their skill level (positive integer)?");
 
-                getMember.SkillLevel = int.Parse(Console.ReadLine());
+                    int skillLevel = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("What is their courage level (0.00 - 2.0)?");
+                    Console.WriteLine("What is their courage level (0.00 - 2.0)?");
 
-                getMember.CourageFactor = double.Parse(Console.ReadLine()); //this is a double, fix
+                    double courageFactor = double.Parse(Console.ReadLine()); //this is a double, fix
 
-                teamMembers.Add(getMember);
+                    TeamMember newMemberHere = new TeamMember(name, skillLevel, courageFactor);
+
+                    teamMembers.Add(newMemberHere);
+                }
+
             }
-            while (getMember.Name != " ");
 
             Console.WriteLine($"Team has {teamMembers.Count} members");
             foreach (TeamMember member in teamMembers)
--- heist/TeamMember.cs
+++ heist/TeamMember.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 
 namespace heist
 {
+
     public class TeamMember
     {
+        public TeamMember (string name, int skill, double courage)
+        {
+            Name = name;
+            SkillLevel = skill;
+            CourageFactor = courage;
+        }
         public string Name {get; set;}
         public int SkillLevel {get; set;}
         public double CourageFactor {get; set;}
