e4ccb149fca30dc54493e37d75c23130dad80832
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-09-26 11:28:33 -0400

    This version does not add members correctly but the loop is almost working.

--- heist/Program.cs
+++ heist/Program.cs
@@ -7,31 +7,38 @@ namespace heist
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Plan Your Heist!");
+
+            //update to store several team members', stop adding members when a blank name is entered, display message with number of members in team, display each team member's info.
+
             TeamMember getMember = new TeamMember();
             List<TeamMember> teamMembers = new List<TeamMember>();
 
+            do
+            {
 
-            Console.WriteLine("Plan Your Heist!");
+                Console.WriteLine("Add team member. What is their name?");
 
-            Console.WriteLine("Add team member. What is their name?");
+                getMember.Name = Console.ReadLine();
 
-            getMember.Name = Console.ReadLine();
+                Console.WriteLine("What is their skill level (positive integer)?");
 
-            Console.WriteLine("What is their skill level (positive integer)?");
+                getMember.SkillLevel = int.Parse(Console.ReadLine());
 
-            getMember.SkillLevel = int.Parse(Console.ReadLine());
+                Console.WriteLine("What is their courage level (0.00 - 2.0)?");
 
-            Console.WriteLine("What is their courage level (0.00 - 2.0)?");
+                getMember.CourageFactor = double.Parse(Console.ReadLine()); //this is a double, fix
 
-            getMember.CourageFactor = double.Parse(Console.ReadLine()); //this is a double, fix
+                teamMembers.Add(getMember);
+            }
+            while (getMember.Name != " ");
 
-            teamMembers.Add(getMember);
-
-            foreach (TeamMember member in teamMembers) 
+            Console.WriteLine($"Team has {teamMembers.Count} members");
+            foreach (TeamMember member in teamMembers)
             {
                 Console.WriteLine($"Member: {member.Name}; Skill: {member.SkillLevel}; Courage: {member.CourageFactor}.");
             }
-            
+
         }
     }
 }
